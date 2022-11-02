d1cfdf48893bebbae74ec7e1f320c82d69552a07
Mario Lisbona <mario.lisbona@gmail.com>
2022-11-02 12:20:45 +1100

    function to revoke/grant admin rights completed

--- controllers/admin_controller.py
+++ controllers/admin_controller.py
@@ -69,31 +69,8 @@ def grant_admin_rights(user_id):
     #admin_access will abort if is_admin is False
     admin_access()
     
-    # ======================================need to fix this function ===========================
-    # return grant_revoke_admin(user_id, True, True, 'already has', 'granted', 'to')
-    
-    #create query statement to return a single Post with the id of the route variable
-    stmt = db.select(User).filter_by(id=user_id)
-    #scalar will return a single post where the id matches post_id and assign the result to the post variable
-    user = db.session.scalar(stmt)
-
-    #if the post exists then change their is_admin attribute to True
-    #else provide an error message and 404 resource not found code
-    if user:
-        #Message displaying that the user already has admin rights
-        if user.is_admin:
-            return {'Message': f'User id: {user_id} \'{user.f_name} {user.l_name}\' already has admin privileges'}
-
-        #user does not have admin rights - change is_admin to True and commit to database
-        user.is_admin = True
-        db.session.commit()
-
-        #return message with new user details
-        return {'Message': f'You successfully granted admin privileges to the user id: {user_id} \'{user.f_name} {user.l_name}\'.',
-        'New user details': UserSchema(exclude=['password']).dump(user)
-        }
-    else:
-        abort(404, description=f'User id:{user_id} does not exist')
+    #call function to grant admin rights
+    return grant_revoke_admin(user_id, True, 'already has', 'granted', 'to')
 
 
 # ======================================Revoke admin rights from any user - ADMIN ONLY==================================
@@ -105,9 +82,14 @@ def revoke_admin_rights(user_id):
     #admin_access will abort if is_admin is False
     admin_access()
 
-    # ======================================need to fix this function ===========================
-    # return grant_revoke_admin(user_id, False, False, 'does not have', 'revoked', 'from')
-    
+    #call function to revoke admin rights
+    return grant_revoke_admin(user_id, False, 'does not have', 'revoked', 'from')
+
+
+# ======================================Function def - Revoke/grant access==================================
+
+
+def grant_revoke_admin(user_id, admin_bool, string_1, string_2, string_3):
     #create query statement to return a single Post with the id of the route variable
     stmt = db.select(User).filter_by(id=user_id)
     #scalar will return a single post where the id matches post_id and assign the result to the post variable
@@ -117,47 +99,17 @@ def revoke_admin_rights(user_id):
     #else provide an error message and 404 resource not found code
     if user:
         #Message displaying that the user already has admin rights
-        if not user.is_admin:
-            return {'Message': f'User id: {user_id} \'{user.f_name} {user.l_name}\' does not have admin privileges'}
+        if admin_bool == user.is_admin:
+            return {'Message': f'User id: {user_id} \'{user.f_name} {user.l_name}\' {string_1} admin privileges'}
 
         #user does not have admin rights - change is_admin to True and commit to database
-        user.is_admin = False
+        user.is_admin = admin_bool
         db.session.commit()
 
         #return message with new user details
-        return {'Message': f'You successfully revoked admin privileges for the user id: {user_id} \'{user.f_name} {user.l_name}\'.',
+        return {'Message': f'You successfully {string_2} admin privileges {string_3} the user id: {user_id} \'{user.f_name} {user.l_name}\'.',
         'New user details': UserSchema(exclude=['password']).dump(user)
         }
     else:
         abort(404, description=f'User id:{user_id} does not exist')
-
-
-
-
-# ======================================Revoke/grant access function==================================
-
-
-# def grant_revoke_admin(user_id, admin_bool, user_admin, string_1, string_2, string_3):
-#     #create query statement to return a single Post with the id of the route variable
-#     stmt = db.select(User).filter_by(id=user_id)
-#     #scalar will return a single post where the id matches post_id and assign the result to the post variable
-#     user = db.session.scalar(stmt)
-
-#     #if the post exists then change their is_admin attribute to True
-#     #else provide an error message and 404 resource not found code
-#     if user:
-#         #Message displaying that the user already has admin rights
-#         if admin_bool user.is_admin:
-#             return {'Message': f'User id: {user_id} \'{user.f_name} {user.l_name}\' {string_1} admin privileges'}
-
-#         #user does not have admin rights - change is_admin to True and commit to database
-#         user.is_admin = user_admin
-#         db.session.commit()
-
-#         #return message with new user details
-#         return {'Message': f'You successfully {string_2} admin privileges {string_3} the user id: {user_id} \'{user.f_name} {user.l_name}\'.',
-#         'New user details': UserSchema(exclude=['password']).dump(user)
-#         }
-#     else:
-#         abort(404, description=f'User id:{user_id} does not exist')
 

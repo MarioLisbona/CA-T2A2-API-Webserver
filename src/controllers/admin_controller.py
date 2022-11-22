df7f7cd3cdd35cf60305af8644e3a3fa9e951285
Mario Lisbona <mario.lisbona@gmail.com>
2022-11-22 14:17:51 +1100

    exlcude replies from display deactivated posts

--- src/controllers/admin_controller.py
+++ src/controllers/admin_controller.py
@@ -84,7 +84,7 @@ def get_all_archived_posts():
     #else display message for no deactivated posts
     if count > 0:
         #use Schema to return json serialized version of the query statement
-        return PostSchema(many=True).dump(posts)
+        return PostSchema(many=True, exclude=['replies']).dump(posts)
     elif count == 0:
         abort(404, description='There are no deactivated posts in the forum')
 
